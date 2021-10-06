825e1c046bd345ce93456aef8f960dc0ecc5d174
unknown <mah_tah30@hotmail.com>
2021-10-06 12:08:42 +0400

    first commit

---
+++ ZwajApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            _context=context;
+        }
+        
+
+       
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+           var Values=await _context.Values.ToListAsync();
+           return Ok(Values);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+           var Value=await _context.Values.FirstOrDefaultAsync(x=>x.id==id);
+           return Ok(Value);
+
+        }
+    }
+}
