03f2fa9e41951ed6e55ecd188fd7dd6209ea29e9
MahmoudTahaKhedr <jetegy@gmail.com>
2021-10-22 22:44:06 +0400

    End Of Section 4

---
+++ ZwajApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+ model:any={};
+  constructor(private authServices:AuthService) { }
+
+  ngOnInit() {
+  }
+  login(){
+    this.authServices.login(this.model).subscribe(
+      next=>{console.log('تم الدخول بنجاح')},
+      error=>{console.log('فشل الدخول')}
+      
+    )
+  }
+  loggedIn(){
+    const token = localStorage.getItem('token')
+    return !! token
+  }
+
+  loggedOut(){
+    localStorage.removeItem('token')
+    console.log('تم الخروج')
+  }
+
+}
